2a2c5539f7ad0bc7b9556418696ae140278d952c
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2022-06-17 17:55:33 -0700

    Add files via upload

--- WMAD202 Sample 1/wmad202-assignment3/src/ca/ciccc/wmad/assignment3/problem6/problem6.java
+++ WMAD202 Sample 1/wmad202-assignment3/src/ca/ciccc/wmad/assignment3/problem6/problem6.java
@@ -1,2 +1,35 @@
-package ca.ciccc.wmad.assignment3.problem6;public class problem6 {
+package ca.ciccc.wmad.assignment3.problem6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class problem6 {
+    public static void remomveDuplicated(){
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int number = input.nextInt();
+        while (number < 100) {
+            list.add(number);
+            number = input.nextInt();
+        }
+
+
+        System.out.println(firstList(list));
+    }
+    public static ArrayList<Integer> firstList(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if ((list.get(j) == list.get(i)) && (i != j)) {
+                    list.remove(j);
+                }
+            }
+        }
+        return list;
+    }
+    }
+
+    private static boolean firstList(ArrayList<Integer> list) {
+        return false;
+    }
 }
