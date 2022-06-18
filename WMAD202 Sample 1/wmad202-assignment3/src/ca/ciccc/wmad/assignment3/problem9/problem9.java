29879ab9c03924a55219f5ba3938fa4793cfef25
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2022-06-17 19:52:04 -0700

    Add files via upload

--- WMAD202 Sample 1/wmad202-assignment3/src/ca/ciccc/wmad/assignment3/problem9/problem9.java
+++ WMAD202 Sample 1/wmad202-assignment3/src/ca/ciccc/wmad/assignment3/problem9/problem9.java
@@ -1,4 +1,39 @@
 package ca.ciccc.wmad.assignment3.problem9;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class problem9 {
+    public static void main(){
+        ArrayList<Integer> newList= makeList();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter an index number.");
+        int index = input.nextInt();
+        ArrayList<Integer> primeList= primeVisibility(newList,index);
+        System.out.printf(" “prime visibility” of the index %d is %d",index,primeList.size());
+
+
+    }
+    public static ArrayList<Integer> primeVisibility(ArrayList<Integer> arrayList, int index ){
+        ArrayList<Integer> primeList = new ArrayList<>();
+        for (int i = 0;i<= index;i++){
+            if (Question1.isPrime(arrayList.get(i))){
+                primeList.add(arrayList.get(i));
+
+            }
+        }
+        return primeList;
+    }
+    public static ArrayList<Integer> makeList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Please enter numbers: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        while (number < 100) {   // Stops when enter greater than 100 and makes list
+            list.add(number);
+            number = input.nextInt();
+        }
+        return list;
+
+    }
 }
