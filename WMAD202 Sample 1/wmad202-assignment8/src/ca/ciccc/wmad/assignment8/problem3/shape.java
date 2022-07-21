280b3921af05a7bb0f48bcf25977693b2b64804d
Armando <107088320+ArmandoRivera00@users.noreply.github.com>
2022-07-20 19:40:39 -0700

    Add files via upload

--- WMAD202 Sample 1/wmad202-assignment8/src/ca/ciccc/wmad/assignment8/problem3/shape.java
+++ WMAD202 Sample 1/wmad202-assignment8/src/ca/ciccc/wmad/assignment8/problem3/shape.java
@@ -1,4 +1,16 @@
 package ca.ciccc.wmad.assignment8.problem3;
 
-public class shape {
+import ca.ciccc.wmad.assignment8.problem3.applicationdriver
+public abstract class shape {
+    public applicationdriver.ShapeType shapes;
+    int[] sides;
+
+    public Shape(applicationdriver.ShapeType shapes, int[] sides) {
+        this.shapes = shapes;
+        this.sides = sides;
+    }
+
+    public abstract float perimeter();
+    public abstract float area();
+
 }
--- WMAD202 Sample 1/wmad202-assignment8/src/ca/ciccc/wmad/assignment8/problem4/problem4.java
+++ WMAD202 Sample 1/wmad202-assignment8/src/ca/ciccc/wmad/assignment8/problem4/problem4.java
@@ -1,4 +1,27 @@
 package ca.ciccc.wmad.assignment8.problem4;
+import java.util.Arrays;
 
 public class problem4 {
+    static class Solution {
+        public static int[] slution(int[] A, int K) {
+            for(int i = 0; i < K; i++){
+
+                int last = A[A.length-1];
+
+                for(int j= A.length-1; j > 0; j--){
+
+                    A[j] = A[j-1];
+                }
+                A[0] = last;
+            }
+            return A;
+        }
+    }
+    public static void main(String[] args) {
+        int[] A= {3, 8, 9, 7, 6};
+        int K 4;
+
+        System.out.println(Arrays.toString(A));
+        System.out.println(Arrays.toString(Solution.solution(A,K)));
+    }
 }
